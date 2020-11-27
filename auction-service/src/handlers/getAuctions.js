d71689dbe561b416501400d2d01420bf04ba5c75
Yordi Ogando <yordiogando@gmail.com>
2020-11-27 11:24:31 -0400

    Added filtering by status feature on gettAuctions

--- auction-service/src/handlers/getAuctions.js
+++ auction-service/src/handlers/getAuctions.js
@@ -6,13 +6,22 @@ const dynamoDB = new AWS.DynamoDB.DocumentClient();
 
 async function getAuctions(event, context) {
   let auctions;
+  const { status } = event.queryStringParameters;
+
+  const params = {
+    TableName: process.env.AUCTIONS_TABLE_NAME,
+    IndexName: "statusAndEndDate",
+    KeyConditionExpression: "#status = :status",
+    ExpressionAttributeValues: {
+      ":status": status,
+    },
+    ExpressionAttributeNames: {
+      "#status": "status",
+    },
+  };
 
   try {
-    const result = await dynamoDB
-      .scan({
-        TableName: process.env.AUCTIONS_TABLE_NAME,
-      })
-      .promise(); // We need to convert this to a promise in order to be able to use "await"
+    const result = await dynamoDB.query(params).promise(); // We need to convert this to a promise in order to be able to use "await"
 
     auctions = result.Items;
   } catch (error) {
